330a0f313b674571eecba355d8b5ed7c4801432d
Oleksii Kartashov <oleksii.kartashov1@gmail.com>
2025-04-27 19:19:49 +0300

    prepared for deployment2

--- task_manager/settings/prod.py
+++ task_manager/settings/prod.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import dj_database_url
 
 
 from .base import *
@@ -16,26 +17,23 @@ ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST', 'localhost'),
 ]
 
-# Додаємо whitenoise для статичних файлів
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+# add whitenoise for static files
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# URL for connecting to Neon PostgreSQL
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://neondb_owner:npg_seqtygkp5lF6@ep-lucky-frost-a2od4yqd-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require')
 
+# Configuration via dj-database-url
 DATABASES = {
- 'default': {
-   'ENGINE': 'django.db.backends.postgresql',
-   'NAME': os.environ.get('DB_NAME', ''),
-   'USER': os.environ.get('DB_USER', ''),
-   'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-   'HOST': os.environ.get('DB_HOST', 'localhost'),
-   'PORT': os.environ.get('DB_PORT', '5432'),
-   'OPTIONS': {
-     'sslmode': 'require',
-   },
- }
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
 }
 
 # Security settings
